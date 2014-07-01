f4e0401457ca5e914a75c64814caf5b1d1a52cc0
Katie Hoffman <ktahoffman@gmail.com>
2014-07-01 10:12:05 -0400

    update, show, index routes functional

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,8 +1,68 @@
 class PigeonsController < ApplicationController
 
-  get '/pigeons' do 
+  #root
+  get '/' do
+    redirect to('/pigeons')
+  end
+
+  #index
+  get '/pigeons' do
     @pigeons = Pigeon.all
     erb :'pigeons/index'
   end
 
+  #new
+  get '/pigeons/new' do
+    erb :'pigeons/new'
+  end
+
+  #show
+  get '/pigeons/:id' do
+    @pigeon = Pigeon.find_by(:id => params[:id])
+    erb :'pigeons/show'
+  end
+
+  #create
+  post '/pigeons' do
+    p = Pigeon.new
+    p.name = params[:name]
+    p.gender = params[:gender]
+    p.color = params[:color]
+    p.lives = params[:lives]
+    if p.save
+      redirect to('/pigeons')
+    else
+      "Error unable to save!"
+    end
+  end
+
+  #edit
+  get '/pigeons/:id/edit' do
+    @pigeon = Pigeon.find_by(:id => params[:id])
+    erb :'pigeons/edit'
+  end
+
+  #update
+  post '/pigeons/:id/update' do
+    p = Pigeon.find_by(:id => params[:id])
+    p.name = params[:name]
+    p.gender = params[:gender]
+    p.color = params[:color]
+    p.lives = params[:lives]
+    if p.save
+      redirect to('/pigeons')
+    else
+      "Error - unable to save!"
+    end
+  end
+
+  #destroy
+  get '/pigeons/:id/destroy' do
+    p = Pigeon.find_by(:id => params[:id])
+    if p.destroy
+      redirect to('/pigeons')
+    else
+      "Error - unable to destroy!"
+    end
+  end
 end
