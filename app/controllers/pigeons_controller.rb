cb062e3bde2a3e12fbe49efaef62d56e43f53e0b
iwilliams83 <indirajamurath@gmail.com>
2018-01-27 21:15:01 +0000

    Done.

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,2 +1,54 @@
 # create your PigeonsController here
 # it should inherit from ApplicationController
+class PigeonsController < ApplicationController
+  get '/' do
+    redirect '/pigeons'
+  end
+
+  get '/pigeons' do
+    @pigeons = Pigeon.all
+    erb :'pigeons/index'
+  end
+
+  get '/pigeons/new' do
+    erb :'pigeons/new'
+  end
+
+  get '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :'pigeons/show'
+  end
+
+  get '/pigeons/:id/edit' do
+   @pigeon = Pigeon.find(params[:id])
+   erb :'pigeons/edit'
+ end
+
+ post '/pigeons' do
+   Pigeon.create(:name => params["name"], :color => params["color"],
+                 :gender => params["gender"], :lives => params["lives"])
+
+   redirect '/pigeons'
+ end
+
+ #edits pigeon
+ patch '/pigeons/:id' do
+   @pigeon = Pigeon.find(params[:id])
+   @pigeon.name = params["name"]
+   @pigeon.color = params["color"]
+   @pigeon.gender = params["gender"]
+   @pigeon.lives = params["lives"]
+   @pigeon.save
+
+   redirect to "/pigeons/#{@pigeon.id}"
+ end
+
+ delete '/pigeons/:id' do
+   pigeon = Pigeon.find(params[:id])
+   pigeon.destroy
+
+   redirect '/pigeons'
+ end
+
+
+end
--- db/migrate/01_create_pigeons.rb
+++ db/migrate/01_create_pigeons.rb
@@ -1,5 +1,14 @@
 # write your pigeon migration here
 # look at the seed.rb file to see
 # what columns to include
-
+class CreatePigeons < ActiveRecord::Migration
+  def change
+    create_table :pigeons do |p|
+      p.string  :name
+      p.string  :color
+      p.string  :gender
+      p.string  :lives
+    end
+  end
+end
 # remember to inherit from the correct ActiveRecord module
